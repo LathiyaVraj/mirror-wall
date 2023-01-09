cf648a3f5212eee21d0c9a4e16b306311bf6f0d7
LathiyaVraj <vrajlathiya88@gmail.com>
2023-01-09 16:05:16 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/mwall1/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.mwall1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/global.dart
@@ -0,0 +1,23 @@
+class global {
+  static List container = [
+    {
+      "name": "Wikipedia",
+    },
+    {
+      "name": "W3School",
+    },
+    {
+      "name": "Java point",
+    },
+    {
+      "name": "Tutorials Point",
+    }
+  ];
+
+  static List linkpage = [
+    {"link": "https://www.wikipedia.org"},
+    {"link": "https://www.w3schools.com"},
+    {"link": "https://www.javatpoint.com"},
+    {"link": "https://www.tutorialspoint.com"},
+  ];
+}
